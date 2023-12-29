4138de51c84d46fc4adc384e7e5c41750a38109e
tracer-ysyx <tracer@ysyx.org>
2023-12-30 02:38:00 +0800

    > compile NEMU 薛立伟 Linux tianyi-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:38:00 up  4:17,  1 user,  load average: 0.36, 0.49, 0.58

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -276,6 +276,7 @@ Suffix_expr parse_expr(char *e){
 
   for(i=1;i<length;i++){
     if(typeof_token(&tokens[i])!=TK_TYPE_STRUCT&&\
+      typeof_token(&tokens[i])!=TK_TYPE_SINGLE&&\
       typeof_token(&tokens[i])==typeof_token(&tokens[i-1])){
       printf("Error: syntex error near \"%s\"\n",tokens[i].str);
       return ans;
