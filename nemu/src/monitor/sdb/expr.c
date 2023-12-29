cb06d5a0a97f3f9f3b5daf759a269bff75249f2e
tracer-ysyx <tracer@ysyx.org>
2023-12-29 23:46:04 +0800

    > compile NEMU 薛立伟 Linux tianyi-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:46:04 up  1:25,  1 user,  load average: 0.75, 0.58, 0.63

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -142,10 +142,10 @@ static bool make_token(char *e,int *length) {
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
 
-        /*
+        
         Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
             i, rules[i].regex, position, substr_len, substr_len, substr_start);
-        */
+        
         position += substr_len;
 
         is_binary_operator=false;
