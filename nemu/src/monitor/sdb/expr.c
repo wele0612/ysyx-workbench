532a58984e80d70adfdf12e6a7798a73671905ad
tracer-ysyx <tracer@ysyx.org>
2023-12-29 17:25:11 +0800

    > compile NEMU 薛立伟 Linux tianyi-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:25:11 up  5:07,  1 user,  load average: 0.74, 0.69, 1.02

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -163,8 +163,7 @@ static bool make_token(char *e) {
 
         break;
       }
-    }
-    printf("nr_token:%d\n",nr_token);
+    };
 
     if (i == NR_REGEX) {
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
