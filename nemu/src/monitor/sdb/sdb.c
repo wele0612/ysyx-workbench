10be2b9b64f28679d1586c0250aa631ef221ccd3
tracer-ysyx <tracer@ysyx.org>
2023-12-30 00:34:01 +0800

    > compile NEMU 薛立伟 Linux tianyi-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:34:01 up  2:13,  1 user,  load average: 0.93, 0.80, 0.75

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -126,7 +126,7 @@ static int cmd_p(char *args){
     value=expr(args,&success);    
 
     if(success){
-      printf("PC \33[1;36m0x%lx\33[0m => %ld\n",(uint64_t)cpu.pc,(int64_t)value);
+      printf("\33[1;36mPC 0x%lx\33[0m => %ld\n",(uint64_t)cpu.pc,(int64_t)value);
     }else{
       Log("Invalid expression.");
     }
