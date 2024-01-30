975e2d98f145d4dfa4ba3c5ae0e67a1286b4e66c
tracer-ysyx <tracer@ysyx.org>
2024-01-29 22:09:10 -0800

    > generate verilog 薛立伟 Linux tianyi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:09:10 up  1:34,  1 user,  load average: 0.61, 0.77, 0.73

--- npc/playground/src/Top_Module.scala
+++ npc/playground/src/Top_Module.scala
@@ -1,29 +1,14 @@
 import chisel3._
 
 /**
-  * Compute GCD using subtraction method.
-  * Subtracts the smaller from the larger until register y is zero.
-  * value in register x is then the GCD
+ * Bundle for nvboard
   */
+class NVboard_IOs extends Bundle{
+  val ledr=Output(Vec(16,Bool()))
+}
 class Top_Module extends Module {
-  val io = IO(new Bundle {
-    val value1        = Input(UInt(16.W))
-    val value2        = Input(UInt(16.W))
-    val loadingValues = Input(Bool())
-    val outputGCD     = Output(UInt(16.W))
-    val outputValid   = Output(Bool())
-  })
+  val io=IO(new NVboard_IOs)
 
-  val x = Reg(UInt())
-  val y = Reg(UInt())
-
-  when(x > y) { x := x - y }.otherwise { y := y - x }
-
-  when(io.loadingValues) {
-    x := io.value1
-    y := io.value2
-  }
-
-  io.outputGCD   := x
-  io.outputValid := y === 0.U
+  io.ledr(0)=true.B
+  io.ledr(1)=false.B
 }
