17c4273132ff8bb102b9b99f730a6d1ffd7a1af3
tracer-ysyx <tracer@ysyx.org>
2024-01-31 14:17:39 -0800

    > generate verilog ysyx_23060173 薛立伟 Linux tianyi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:17:39 up  4:16,  1 user,  load average: 1.39, 1.54, 1.87

--- npc/chisel_src/src/Top_Module.scala
+++ npc/chisel_src/src/Top_Module.scala
@@ -9,16 +9,14 @@ class NVboard_IOs extends Bundle{
 
    
   def loadDefault():Unit={
-    ledr:=5.U //.asTypeOf(ledr)
+    ledr:=0.U //.asTypeOf(ledr)
   }
 }
 class Top_Module extends Module {
   val io=IO(new NVboard_IOs)
   val nv_sw=VecInit(io.sw.asBools)
-  val nv_ledr=Vec(16,false.B)
 
   io.loadDefault()
-  io.ledr:=nv_ledr.asUInt
 
   io.ledr(0):=Mux(nv_sw(2),nv_sw(1),nv_sw(0))
 
