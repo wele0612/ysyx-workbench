ab1f932b79c3a0cd0039c5df01c0f056a9ce81cc
tracer-ysyx <tracer@ysyx.org>
2024-02-03 15:16:18 -0800

    > compile NEMU ysyx_23060173 薛立伟 Linux tianyi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:16:18 up  3:41,  1 user,  load average: 0.75, 0.66, 0.65

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -124,7 +124,7 @@ static int decode_exec(Decode *s) {
 //RV32M Multiply Extension
   
   INSTPAT("0000001 ????? ????? 000 ????? 01100 11", mul    , R, R(rd) = src1 * src2);
-  INSTPAT("0000001 ????? ????? 001 ????? 01100 11", mulh   , R, R(rd) = ((int64_t)src1 * (int64_t)src2) >> 32);
+  INSTPAT("0000001 ????? ????? 001 ????? 01100 11", mulh   , R, R(rd) = BITS(((int64_t)src1 * (int64_t)src2), 63, 32));
 
 //***********************************************************************************
 
